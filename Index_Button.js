70d111bd05a4652d5b54f2aa12f5b27322e8ddbf
QingboYuezheng <qingboyuezheng@Yuezheng-Qingbos-MacBook-Pro.local>
2024-05-24 10:52:50 -0700

    Initial commit

---
+++ Index_Button.js
@@ -0,0 +1,26 @@
+function linkToChapterZero() {
+    window.location.href = 'zh_Chapter_0.html';
+}
+function linkToChapterOne() {
+    window.location.href = 'zh_Chapter_1.html';
+}
+
+function linkToChapterTwo() {
+    window.location.href = 'zh_Chapter_2.html';
+}
+
+function linkToChapterThree() {
+    window.location.href = 'zh_Chapter_3.html';
+}
+
+function linkToChapterFour() {
+    window.location.href = 'zh_Chapter_4.html';
+}
+
+function linkToChapterFive() {
+    window.location.href = 'zh_Chapter_5.html';
+}
+
+function linkToChapterSix() {
+    window.location.href = 'zh_Chapter_6.html';
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# My-Fiction-Universe-CN
+
+__你好！这是我网站的实现库，HTML、JavaScript和CSS实现请随意借鉴，但是请不要抄袭我的文章__
+
+[_点我直达网站展示(请启用JavaScript)_](https://yuezhengqingbo.github.io/My-Fiction-Universe-CN/))
